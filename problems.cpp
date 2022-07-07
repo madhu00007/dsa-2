51d4f2831c2439cab6784cc8004aeb96b658d281
Mrinal M <mmadhubrata33@gmail.com>
2022-07-07 12:01:05 +0530

    first commit

---
+++ problems.cpp
@@ -0,0 +1,53 @@
+//calculate of average of two numbers.
+/*
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  float num1, num2,sum,avg;
+  cout<<"enter two numbers ";
+  cin>>num1;
+  cin>>num2;
+  sum=num1+num2;
+  avg=sum/2;
+  cout<<"sum of two numbers = "<<sum<<"\n";
+  cout<<"average = "<<avg;
+  return 0;
+}
+*/
+
+//how to find size() of datatypes in C++
+ /*
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout<<"int " <<sizeof(int)<<" bytes"<<endl;
+  cout<<"float " <<sizeof(float)<<" bytes"<<endl;
+  cout<<"char " <<sizeof(char)<<" bytes"<<endl;
+  cout<<"long " <<sizeof(long)<<" bytes"<<endl;
+  cout<<"double " <<sizeof(double)<<" bytes"<<endl;
+}
+ */
+
+//how to find size() of modifiers in C++
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Size of signed int " <<sizeof(signed int)<<" bytes"<<endl;
+  cout<<"Size of signed char " <<sizeof(signed char)<<" bytes"<<endl;
+  cout<<"Size of unsigned int " <<sizeof(unsigned int)<<" bytes"<<endl;
+  cout<<"Size of unsigned char " <<sizeof(unsigned char)<<" bytes"<<endl;
+  cout<<"Size of short  int " <<sizeof(short int)<<" bytes"<<endl;
+  cout<<"Size of long int " <<sizeof(long int)<<" bytes"<<endl;
+  cout<<"Size of long double " <<sizeof(long double)<<" bytes"<<endl;
+  cout<<"Size of signed long int " <<sizeof(signed long int)<<" bytes"<<endl;
+  cout<<"Size of  unsigned short int " <<sizeof( unsigned short int)<<" bytes"<<endl;
+  cout<<"Size of  long long int  " <<sizeof( long long int )<<" bytes"<<endl;
+}
+*/
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+sizeof(unsigned int)<<" bytes"<<endl;
+  cout<<"Size of unsigned char " <<sizeof(unsigned char)<<" bytes"<<endl;
+  cout<<"Size of short  int " <<sizeof(short int)<<" bytes"<<endl;
+  cout<<"Size of long int " <<size
